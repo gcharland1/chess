affdcee33c2f6d67ce90674e6fe133d860ca8473
Gabriel Charland <g.charland1@protonmail.com>
2020-12-28 17:47:33 -0500

    Cleaned main loop with make_move method

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
 import pygame
-import time
 import chess_set
-import chessman
 
 def main():
     set = chess_set.ChessSet()
@@ -19,33 +17,7 @@ def main():
         update_display(root, bg, set, whos_turn)
         for event in pygame.event.get():
             if event.type == pygame.MOUSEBUTTONUP:
-                if (r1, c1) == (-1, -1):
-                    r1, c1 = get_board_index(event.pos)
-                    if not set.is_chessman(r1, c1):
-                        r1, c1 = -1, -1
-                        print('Please select a piece in order to move it!')
-                    else:
-                        color = set.board[r1][c1].color
-                        if not color == whos_turn:
-                            print(f'Wrong team! Look at caption to see who''s turn it is.')
-                            r1, c1 = -1, -1
-                else:
-                    r2, c2 = get_board_index(event.pos)
-                    if set.is_chessman(r2, c2):
-                        color = set.board[r2][c2].color
-                    else:
-                        color = ''
-
-                    if not (r1, c1) == (r2, c2) and not color == whos_turn:
-                        set.move(r1, c1, r2, c2)
-                        if whos_turn == 'w':
-                            whos_turn = 'b'
-                        else:
-                            whos_turn = 'w'
-                    else:
-                        print('Canceled move')
-                    r1, c1 = -1, -1
-
+                r1, c1, whos_turn = make_move(set, event.pos, whos_turn, r1, c1)
 
             if event.type == pygame.QUIT:
                 running = False
@@ -73,6 +45,38 @@ def get_board_index(pos):
     c = int(pos[0]/SQ_G)
     r = int(pos[1]/SQ_H)
     return (r, c)
+
+def make_move(set, event_pos, whos_turn, r1, c1):
+    if (r1, c1) == (-1, -1):
+        r1, c1 = get_board_index(event_pos)
+        if not set.is_chessman(r1, c1):
+            r1, c1 = -1, -1
+            print('Please select a piece in order to move it!')
+        else:
+            color = set.board[r1][c1].color
+            if not color == whos_turn:
+                print(f'Wrong team! Look at caption to see who''s turn it is.')
+                r1, c1 = -1, -1
+    else:
+        r2, c2 = get_board_index(event_pos)
+        if set.is_chessman(r2, c2):
+            color = set.board[r2][c2].color
+        else:
+            color = ''
+
+        if not (r1, c1) == (r2, c2) and not color == whos_turn:
+            set.move(r1, c1, r2, c2)
+            if whos_turn == 'w':
+                whos_turn = 'b'
+            else:
+                whos_turn = 'w'
+        else:
+            print('Canceled move')
+        r1, c1 = -1, -1
+
+    return r1, c1, whos_turn
+
+
 
 IMAGE_DIR = './images/'
 BG_IMAGE = 'chessboard.png'
